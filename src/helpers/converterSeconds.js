130a1aa98e5679a5ebdd9735ca90241a86bcbe47
Valen <v.lunaklick@gmail.com>
2022-01-04 22:57:40 -0300

    changed bug in timer, added firebes to the html

--- src/helpers/converterSeconds.js
+++ src/helpers/converterSeconds.js
@@ -1,8 +1,8 @@
 export function convertSeconds(time) {
 	let seconds, minutes
 
-	10 > time % 60 ? (seconds = `0${time % 60}`) : (seconds = time % 60)
-	10 > time / 60 - (time % 60)
+	10 >= time % 60 ? (seconds = `0${time % 60}`) : (seconds = time % 60)
+	10 >= time / 60
 		? (minutes = `0${Math.floor(time / 60)}`)
 		: (minutes = Math.floor(time / 60))
 
