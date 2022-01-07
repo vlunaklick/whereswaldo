3c448615fb5e285ed1fc3b070f63808db9ccfe8b
Valen <v.lunaklick@gmail.com>
2022-01-07 12:14:30 -0300

    removed script firebase from html and changed firebase things in uploadToDb

--- src/helpers/uploadToDB.js
+++ src/helpers/uploadToDB.js
@@ -1,5 +1,6 @@
 import db from '../firebase/config'
-import firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
 const uploadToDB = scores => {
 	let firstMap = db.collection('maps').doc('firstmap')
